1e373068e4c8db3d94603792ee2dcec9c1aa986b
Dinesh Kumar <dinesh.kumar@teaminc.com>
2020-03-27 18:15:56 +0530

    Creating a sample web site

---
+++ CodeCoverageDemoProject1/CodeCoverageDemoProject1/bin/Debug/CodeCoverageDemoProject/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCoverageDemoProject
+{
+    public class Class1
+    {
+        public int LargestNumberInArray(int[] a)
+        {
+            int largest = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > largest)
+                {
+                    largest = a[i];
+                }
+
+            }
+            return largest;
+        }
+    }
+}
---
+++ TestSite01/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CodeCoverageDemoProject1;
+
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int[] a = new int[5];
+        a[0] = Int32.Parse(TextBox1.Text);
+        a[1] = Int32.Parse(TextBox2.Text);
+        a[2] = Int32.Parse(TextBox3.Text);
+        a[3] = Int32.Parse(TextBox4.Text);
+        a[4] = Int32.Parse(TextBox5.Text);
+        Program pr = new Program();
+        Label1.Text = pr.LargestNumberInArray(a).ToString();
+
+    }
+    
+}
